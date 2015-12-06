a701e7831b5e1b338225ead5ec8a52a31a35a9b2
root <root@beaglebone.(none)>
2015-12-06 02:25:21 +0000

    add new .gitignoer

--- services/weather.js
+++ services/weather.js
@@ -11,8 +11,8 @@ function Weather() {
     request({  
 //	    url:'http://api.openweathermap.org/data/2.5/weather?q=del+mar,us&APPID=f97bcd1cb2dff0efc0e8c17b0d1742eb',
 	    url:'http://api.openweathermap.org/data/2.5/weather?q=del+mar,us&APPID=f97bcd1cb2dff0efc0e8c17b0d1742eb',
-		port: '10015',                                 // Philips proxy port
-        proxy: 'http://199.168.151.10'                 // Philips proxy url
+//		port: '10015',                                 // Philips proxy port
+//        proxy: 'http://199.168.151.10'                 // Philips proxy url
     }, 
 
         function (error, response, body) {
