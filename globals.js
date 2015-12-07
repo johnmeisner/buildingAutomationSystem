74c6e8ad9253b546b0738c46bdcc96bf1f665c6f
John Meisner <jhn_msnr@yahoo.com>
2015-12-06 19:38:59 -0800

    Delete globals.js

--- globals.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports = function () {
-return {
-	weatherUrl: 'http://api.openweathermap.org/data/2.5/weather?q=del+mar,us&APPID=f97bcd1cb2dff0efc0e8c17b0d1742eb',
-	port: '10015',
-	proxy: 'http://199.168.151.10'
-	};
-};
